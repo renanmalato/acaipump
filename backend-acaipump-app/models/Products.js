ace470c71daf415cd9b642efac21f8a476e37d4d
Renan Malato <malatows@gmail.com>
2024-05-15 17:26:47 -0400

    number

--- backend-acaipump-app/models/Products.js
+++ backend-acaipump-app/models/Products.js
@@ -4,8 +4,8 @@ const ProductSchema = new mongoose.Schema({
 
     title: {type: String, required: true},
     subtitle: {type: String, required: true},
-    unity: {type: String, required: true},
-    price: {type: String, required: true},
+    unity: {type: Number, required: true},
+    price: {type: Number, required: true},
     imageUrl: {type: String, required: true},
     description: {type: String, required: true},
     product_location: {type: String, required: true},
