8db4594c6b39da44d354a94086b4100b3026a331
Renan Malato <malatows@gmail.com>
2024-05-15 18:32:52 -0400

    test item price

--- payment_server/routes/stripe.js
+++ payment_server/routes/stripe.js
@@ -49,7 +49,7 @@ router.post("/create-checkout-session", async (req, res) => {
             id: item.id,
           },
         },
-        unit_amount: item.price * 100,
+        unit_amount: 2000,
       },
       quantity: item.cartQuantity,
     };
