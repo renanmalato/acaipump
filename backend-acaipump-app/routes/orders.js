51845e948ec511fac0ae14bc19601c42b9bac5d3
Renan Malato <malatows@gmail.com>
2024-05-16 14:48:20 -0400

    getOrders

--- backend-acaipump-app/routes/orders.js
+++ backend-acaipump-app/routes/orders.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const ordersController = require('../controllers/ordersController');
 const { verifyToken } = require('../middleware/verifyToken');
 
-router.get("/",verifyToken, ordersController.getUserOrders);
+router.get("/",verifyToken, ordersController.getOrders);
+router.get("/:id",verifyToken, ordersController.getUserOrders);
 
 module.exports = router
 
