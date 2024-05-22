432477968d2c262368ac5531f994ca657447002d
Renan Malato <malatows@gmail.com>
2024-05-22 15:41:47 -0400

    cart commits

--- frontend-acaipump-app/screens/Cart.jsx
+++ frontend-acaipump-app/screens/Cart.jsx
@@ -294,6 +294,7 @@ const Cart = ({ navigation }) => {
                           />
                         </TouchableOpacity>
                       </View>
+
                     </>
                   }
                 />
